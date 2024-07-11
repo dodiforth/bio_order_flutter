1c90bf87100e9cf89e2422f3c919345a297c92c2
Dowon Kim <dowoniscool@yahoo.com>
2024-07-11 15:28:50 +0200

    Chore: Add Router feature by go_router dependency

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -47,7 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) => CartScreen(),
+                  builder: (context) => CartScreen(uid: "",),
                 ),
               );
             },
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,62 @@
+import 'package:e_product_order_flutter/home/cart_screen.dart';
 import 'package:e_product_order_flutter/home/home_screen.dart';
 import 'package:e_product_order_flutter/home/product_add_screen.dart';
+import 'package:e_product_order_flutter/home/product_detail_screen.dart';
 import 'package:e_product_order_flutter/login/login_screen.dart';
 import 'package:e_product_order_flutter/login/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final router = GoRouter(
+    initialLocation: "/login",
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => HomeScreen(),
+        routes: [
+          GoRoute(
+            path: "cart/:uid",
+            builder: (context, state) => CartScreen(
+              uid: state.pathParameters["uid"] ?? "",
+            ),
+          ),
+          GoRoute(
+            path: "product",
+            builder: (context, state) => ProductDetailScreen(),
+          ),
+          GoRoute(
+            path: "product/add",
+            builder: (context, state) => ProductAddScreen(),
+          ),
+        ],
+      ),
+      GoRoute(
+        path: "/login",
+        builder: (context, state) => LoginScreen(),
+      ),
+      GoRoute(
+        path: "/sign_up",
+        builder: (context, state) => SignUpScreen(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'BIO ORDER',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      routerConfig: router,
     );
   }
 }
